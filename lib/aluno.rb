c21fc7bc7626d97b5966aad8d7ad7b84db4f2c8e
Lauro Caetano <laurocaetano1@gmail.com>
2012-05-15 23:08:26 -0300

    Update lib/aluno.rb

--- lib/aluno.rb
+++ lib/aluno.rb
@@ -1,11 +1,8 @@
 class Aluno
+  attr_reader :nome, :quantidade_de_exercicios_resolvidos
 
-	attr_reader :nome, :quantidade_de_exercicios_resolvidos
-
-	def initialize(nome, quantidade_de_exercicios_resolvidos)
-		@nome = nome
-		@quantidade_de_exercicios_resolvidos = quantidade_de_exercicios_resolvidos
-	end
-	
-	
+  def initialize(nome, quantidade_de_exercicios_resolvidos)
+    @nome = nome
+    @quantidade_de_exercicios_resolvidos = quantidade_de_exercicios_resolvidos
+  end
 end
